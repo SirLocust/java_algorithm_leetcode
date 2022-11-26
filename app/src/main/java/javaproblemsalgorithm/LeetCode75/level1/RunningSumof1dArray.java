b08fd2a4fda1fdd2eb6320852baccc2ac35c5c09
Juan Gomez Castillo <juan.gomezc@sofka.com.co>
2022-11-25 20:37:31 -0500

    basic solve Running Sum of 1d Array

--- app/src/main/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArray.java
+++ app/src/main/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArray.java
@@ -3,8 +3,16 @@ package javaproblemsalgorithm.LeetCode75.level1;
 public class RunningSumof1dArray {
 
   public int[] runningSum(int[] nums) {
-   
-    return nums;
+    int[] result = new int[nums.length];
+    int lenght = nums.length;
+    int pos = 0;
+    int sum = 0;
+    while(pos<lenght){
+      sum += nums[pos];
+      result[pos] = sum;
+      pos++;
+    } 
+    return result;
   }
   
 }
--- app/src/test/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArrayTest.java
+++ app/src/test/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArrayTest.java
@@ -1,10 +1,28 @@
 package javaproblemsalgorithm.LeetCode75.level1;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class RunningSumof1dArrayTest {
+
+  
   @Test
   void testRunningSum() {
 
+    RunningSumof1dArray problem = new RunningSumof1dArray();
+    int input[] = {3,1,2,10,1} ;
+    int output[] = {3,4,6,16,17} ;
+
+    int[] result = problem.runningSum(input);
+    assertArrayEquals(output,result);
+
   }
+
+  
+
 }
