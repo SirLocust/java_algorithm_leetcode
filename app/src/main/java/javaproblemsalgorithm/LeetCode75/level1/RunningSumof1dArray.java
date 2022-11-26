7852946bf079a996860fa499256614595aa63ebb
Juan Gomez Castillo <juan.gomezc@sofka.com.co>
2022-11-25 19:36:24 -0500

    basic repo for solve problems leetcode

---
+++ app/src/main/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArray.java
@@ -0,0 +1,10 @@
+package javaproblemsalgorithm.LeetCode75.level1;
+
+public class RunningSumof1dArray {
+
+  public int[] runningSum(int[] nums) {
+   
+    return nums;
+  }
+  
+}
---
+++ app/src/test/java/javaproblemsalgorithm/LeetCode75/level1/RunningSumof1dArrayTest.java
@@ -0,0 +1,10 @@
+package javaproblemsalgorithm.LeetCode75.level1;
+
+import org.junit.jupiter.api.Test;
+
+public class RunningSumof1dArrayTest {
+  @Test
+  void testRunningSum() {
+
+  }
+}
